1c368957a86dada22a3500da11c652e81f5ca4db
Robbie Huffman <robbie.huffman@gmail.com>
2015-01-16 17:39:21 -0500

    add clojars repo info

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
-(defproject hypertext-ray "0.1.0"
+(defproject 
+  hypertext-ray "0.1.0"
   :description "An HTML disencoder, giving you a view into the data behind the markup."
+  :url "https://github.com/robbieh/hyptertext-ray"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                  [clj-webdriver "0.6.1"]])
+                 [clj-webdriver "0.6.1"]]
+  :repositories  [["releases"  {:url "https://clojars.org/repo"
+                                :creds :gpg}]]
+
+  )
