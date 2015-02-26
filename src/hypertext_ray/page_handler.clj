d88a583951f959962d8fc860289eee3c6900d381
Robbie Huffman <robbie.huffman@gmail.com>
2015-02-25 23:41:22 -0500

    first version of page handler

--- src/hypertext_ray/page_handler.clj
+++ src/hypertext_ray/page_handler.clj
@@ -1,9 +1,29 @@
 (ns hypertext-ray.page-handler
-  (:use [clj-webdriver.taxi])
+  (:use [clj-webdriver.taxi]
+     [hypertext-ray re-maps finders explore navigation login])
   
   )
 
-; page-hints, to idenitfy current page
-; front
-; user agreement
-;
+
+
+(defn action-dispatch [siteinfo action current-page]
+  (case action
+    :login (do-login siteinfo)
+    :wait (println "wait")
+    :collect-data (find-data siteinfo current-page)
+    :logout (println "logout")
+    :quit (quit)
+    siteinfo
+    )
+  )
+
+(defn handle-page [siteinfo]
+  (let [current-page (match-page siteinfo)
+        action-list (current-page (:pageactions siteinfo))
+        ]
+    (println current-page action-list)
+    (reduce #(action-dispatch % current-page) siteinfo action-list)
+    
+    )
+  )
+
